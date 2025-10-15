31c0d595571cdafa35a4faf620768d8e511ee2f0
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-10-15 23:10:35 +0900

    update: fixed model and params

--- train/main.py
+++ train/main.py
@@ -14,19 +14,20 @@ def train():
         print("WANDB_API_KEY not found in environment variables. Skipping wandb login.")
         raise EnvironmentError("WANDB_API_KEY not found in environment variables.")
 
-    model = YOLO("yolo12l.yaml")
+    model = YOLO("yolo12m.yaml")
     data_path = "dataset/data.yaml"
     model.train(
         data=data_path,
         epochs=300,
         imgsz=640,
-        batch=64,
+        batch=96,
         optimizer="AdamW",
-        lr0=0.001,
-        name="yolov12l",
+        lr0=0.0001,
+        amp=True,
+        name="yolo12m",
         project=f"fig-detect-yolo",
     )
-    model.save("yolov12l-fig-detect.pt")
+    model.save("yolo12m-fig-detect.pt")
 
 
 if __name__ == "__main__":
